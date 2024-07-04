b09adfa5f1e0220b7f3e3cf690bca66cf569e503
Venz00-cryptic <tkanao91@gmail.com>
2024-07-04 17:15:44 +0800

    Exam1

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 // script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('.navbar a');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const targetSection = this.getAttribute('href').substring(1);
-            scrollToSection(targetSection);
-        });
-    });
-
-    function scrollToSection(sectionId) {
-        const section = document.getElementById(sectionId);
-        if (section) {
-            section.scrollIntoView({ behavior: 'smooth' });
-        }
-    }
-});
+function greet() {
+    console.log("Test Hello");
+}
